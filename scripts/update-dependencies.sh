42fcb9c2f6182ac6fd74889071e730ffad3d62a2
Le Vivilet <levivilet@pm.me>
2026-07-24 15:09:25 +0200

    feature: migrate to npm workspaces (#3)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,19 +16,13 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x jest -x @jest/globals -x lerna -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x jest -x @jest/globals -x typescript
 }
 
                                                        updateDependencies             &&
 cd packages/build                                   && updateDependencies && cd ../.. &&
 cd packages/file-permission-process                 && updateDependencies && cd ../.. &&
+npm install                                         &&
 
 echo "Great Success!"
 
